e958b5010f0cb5fe80a5c17d3c0f6f540c604ba4
IsaiahBonilla <allinobid@gmail.com>
2025-02-26 17:23:08 +0000

    Wed, Feb 26, 2025, 9:23 AM -08:00

--- js/loading.js
+++ js/loading.js
@@ -3,9 +3,13 @@ const bg = document.querySelector('.bg')
 
 let load = 0
 
+let int = setInterval(blurring, 300)
 
 function blurring(){
     load = load +1
+    if (load > 99){
+        clearInterval(int)
+    }
     loadText.innerText = `${load}%`
 }
 
