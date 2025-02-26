e4b8e15cb93886e19148dcdfef541a0d7331f66b
IsaiahBonilla <allinobid@gmail.com>
2025-02-26 17:28:36 +0000

    Wed, Feb 26, 2025, 9:28 AM -08:00

--- js/loading.js
+++ js/loading.js
@@ -6,10 +6,11 @@ let load = 0
 let int = setInterval(blurring, 300)
 
 function blurring(){
-    load = load +1
+    load = load + 1
     if (load > 99){
         clearInterval(int)
     }
-    loadText.innerText = `${load}%`
-}
 
+    loadText.innerText =`${load}%`
+    bg.style.filter = `blur(${load})`
+}
