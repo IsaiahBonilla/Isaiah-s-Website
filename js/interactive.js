e08739e963777c9ac01977aef1abd0784d72b619
IsaiahBonilla <allinobid@gmail.com>
2025-03-14 16:57:24 +0000

    Fri, Mar 14, 2025, 9:57 AM -07:00

--- js/interactive.js
+++ js/interactive.js
@@ -2,6 +2,7 @@ const buttons = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '*', '%', '+', '-'
 const element1 = document.getElementById('element1')
 buttons.forEach(button => {
     const btn = document.createElement('button')
+    const buttonPressed = document.createElement('pres')
     btn.classList.add('btn')
     btn.innerHTML = button
     btn.addEventListener('click', () => {
