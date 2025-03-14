d3524e8c4570f2797dbc5c020b70e20a5812c3a8
IsaiahBonilla <allinobid@gmail.com>
2025-03-14 16:35:21 +0000

    Fri, Mar 14, 2025, 9:35 AM -07:00

--- js/event.js
+++ js/event.js
@@ -7,7 +7,7 @@ element1.addEventListener('click',()=>{
     element1.innerHTML = "I've been clicked! <br> Try double clicking me."
 })
 element1.addEventListener('dblclcik', ()=>{
-    element1.ineerHTML = "I've been double clicked! <br> Try single clicking me."
+    element1.innerHTML = "I've been double clicked! <br> Try single clicking me."
 })
 
 element2.addEventListener('mouseover', ()=>{
--- js/interactive.js
+++ js/interactive.js
@@ -1,11 +1,12 @@
 const buttons = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '*', '%', '+', '-']
-
+const element1 = document.getElementById('element1')
 buttons.forEach(button => {
     const btn = document.createElement('button')
     btn.classList.add('btn')
     btn.innerHTML = button
     btn.addEventListener('click', () => {
         document.getElementById('buttons')
+        element1.innerHTML = button
     })
     document.getElementById('buttons').appendChild(btn)
 })
