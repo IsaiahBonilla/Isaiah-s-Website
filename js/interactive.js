986b1b329043c7771b9500aa0208694f2455a668
IsaiahBonilla <allinobid@gmail.com>
2025-03-15 13:42:03 -0700

    updated

--- js/interactive.js
+++ js/interactive.js
@@ -2,13 +2,16 @@ const buttons = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '*', '%', '+', '-'
 const element1 = document.getElementById('element1')
 buttons.forEach(button => {
     const btn = document.createElement('button')
-    const buttonPressed = document.createElement('pres')
+    var buttonPressed = document.createElement('pressed')
     btn.classList.add('btn')
     btn.innerHTML = button
     btn.addEventListener('click', () => {
+        buttonPressed = button;
         document.getElementById('buttons')
-
-        element1.innerHTML = "button"
+        if (btn == buttonPressed){
+            element1.classList.add('active')
+            element1.innerHTML = buttons.buttonPressed
+        }
     })
     document.getElementById('buttons').appendChild(btn)
 })
