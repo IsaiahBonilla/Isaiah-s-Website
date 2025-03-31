c002437f701d22d31aa8c880f91a0d878e029100
IsaiahBonilla <allinobid@gmail.com>
2025-03-31 17:00:21 +0000

    Mon, Mar 31, 2025, 10:00 AM -07:00

--- js/relaxer.js
+++ js/relaxer.js
@@ -1,14 +1,21 @@
 const container = document.getElementById('container')
 const text = document.getElementById('text')
 
+breathAnimation()
+breathAnimation()
 
 function breathAnimation() {
     text.innerHTML = 'Breath In'
+    container.classList.remove('shrink')
     container.classList.add('grow')
 
     setTimeout( () => {
         text.innerHTML = 'Hold'
     }, 3000)
 
-    setTimeout(?, 4500)
+    setTimeout( () => {
+        text.innerHTML = 'Breath Out'
+        container.classList.remove('grow')
+        container.classList.add('shrink')
+    }, 4500)
 }
