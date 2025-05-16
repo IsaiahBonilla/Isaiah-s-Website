39125050e9c3b113655c2659c5b24e2f78a55957
IsaiahBonilla <allinobid@gmail.com>
2025-05-16 17:12:07 +0000

    Fri, May 16, 2025, 10:12 AM -07:00

--- js/speach.js
+++ js/speach.js
@@ -5,3 +5,53 @@ const readBtn = document.getElementById('read')
 const toggleBtn = document.getElementById('toggle')
 const closeBtn = document.getElementById('close')
 
+const data = [
+    {
+        image: '../drink.jpg',
+        text: "I'm Thirsty"
+    },
+    {
+        image: '../food.jpg',
+        text: "I'm Hungry"
+    },
+    {
+        image: '../tired.jpg',
+        text: "I'm Tired"
+    },
+    {
+        image: '../hurt.jpg',
+        text: "I'm Hurt"
+    },
+    {
+        image: '../happy.jpg',
+        text: "I'm Happy"
+    },
+    {
+        image: '../angry.jpg',
+        text: "I'm Angry"
+    },
+    {
+        image: '../sad.jpg',
+        text: "I'm Sad"
+    },
+    {
+        image: '../scared.jpg',
+        text: "I'm Scared"
+    },
+    {
+        image: '../outside.jpg',
+        text: "I want to go outside..."
+    },
+    {
+        image: '../home.jpg',
+        text: "I want to go home..."
+    },
+    {
+        image: '../school.jpg',
+        text: "I want to go to school..."
+    },
+    {
+        image: '../grandma.jpg',
+        text: "I want to go to grandma's..."
+    }
+]
