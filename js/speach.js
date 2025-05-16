2f6d415e19e80c5f4cd4e7adbcd1c8140267dab4
IsaiahBonilla <allinobid@gmail.com>
2025-05-16 17:03:45 +0000

    Fri, May 16, 2025, 10:03 AM -07:00

---
+++ js/speach.js
@@ -0,0 +1,7 @@
+const main = document.quereySelector('main')
+const voicesSelect = document.getElementById('voices')
+const textarea = document.getElementById('text')
+const readBtn = document.getElementById('read')
+const toggleBtn = document.getElementById('toggle')
+const closeBtn = document.getElementById('close')
+
